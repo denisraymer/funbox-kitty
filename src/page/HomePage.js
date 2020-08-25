6f6ee972e0200187773052b7ff9790ac9eb3896a
DenisRaymer <to.mars.66.miles@gmail.com>
2020-08-25 23:01:24 +0300

    Added homepage title

--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -3,6 +3,7 @@ import React from 'react';
 const HomePage = () => {
     return (
         <div className='card-block'>
+            <div className="card-block__title">Ты сегодня покормил кота?</div>
             <div className="card-block__stroke">
                 <div className="card-block__item"/>
                 <div className="card-block__item"/>
