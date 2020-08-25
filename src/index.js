2bfe4a0850aced09da2d7c10aa2e7f8b582d7769
DenisRaymer <to.mars.66.miles@gmail.com>
2020-08-25 21:19:58 +0300

    Sorted folder with styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/styles/index.scss';
+import './assets/styles/main.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
