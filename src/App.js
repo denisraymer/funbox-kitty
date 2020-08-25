522af3b89c3a9971df3da4bbbcb4b4ad2ce8f530
DenisRaymer <to.mars.66.miles@gmail.com>
2020-08-25 19:12:21 +0300

    Added home page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import React from 'react';
+import HomePage from './page/HomePage';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <HomePage/>
     );
 }
 
