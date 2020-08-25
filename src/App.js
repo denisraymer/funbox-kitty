d9faac4c66ffb56589e874c0fd0a8f9c792212c2
DenisRaymer <to.mars.66.miles@gmail.com>
2020-08-25 21:28:46 +0300

    Created routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import HomePage from './page/HomePage';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
+import Page404 from './page/404';
 
 function App() {
     return (
-        <HomePage/>
+        <Router>
+            <Switch>
+                <Route exact path='/' component={HomePage}/>
+                <Route component={Page404}/>
+            </Switch>
+        </Router>
     );
 }
 
