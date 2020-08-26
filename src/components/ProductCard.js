8010d498ec17e2de42db0aec13de5faefdc6edd3
DenisRaymer <to.mars.66.miles@gmail.com>
2020-08-26 12:24:17 +0300

    Added layout content in product card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,11 +2,20 @@ import React from 'react';
 
 const ProductCard = () => {
     return (
-        <div className="product-card">
-            <div className='product-card__content'/>
-            <div className='product-card__footer'>
-                Чего сидишь? Порадуй котэ, купи.
+        <div className='product-card product-card_selected'>
+            <div className='product-card__content'>
+                <div className='product-card__description'>Сказочное заморское яство</div>
+                <div className='product-card__title'>Нямушка</div>
+                <div className='product-card__category'>с фуа-гра</div>
+                <ul className='product-card__characteristics'>
+                    <li>10 порций</li>
+                    <li>мышь в подарок</li>
+                </ul>
+                <img src={require('../assets/images/content/product-kitty.png')} alt='Kitty'
+                     className='product-card__image'/>
+                <div className="product-card__price">0,5 <span className='unit'>кг</span></div>
             </div>
+            <div className='product-card__footer'>Чего сидишь? Порадуй котэ, купи.</div>
         </div>
     )
 }
