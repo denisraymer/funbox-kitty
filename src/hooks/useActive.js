ed6d32d54d192ddc9ae473e34a3062eb9853af75
DenisRaymer <to.mars.66.miles@gmail.com>
2020-08-27 19:07:52 +0300

    Fixed logic behavior of the card adding

--- src/hooks/useActive.js
+++ src/hooks/useActive.js
@@ -5,8 +5,17 @@ export default ({isActive: disabled, category}) => {
     const [productMessage, setProductMessage] = useState('Чего сидишь? Порадуй котэ, ');
     const [productStatus, setProductStatus] = useState('');
 
+    useEffect(() => {
+        if (!disabled) {
+            setProductMessage(`Печалька, с ${category} закончился.`);
+            setProductStatus('product-card_disabled');
+        }
+    }, []);
+
     const addProduct = () => {
-        if (!isActiveProduct) {
+        if (!disabled) {
+            return null
+        } else if (!isActiveProduct) {
             setProductStatus('product-card_selected');
             setProductMessage('Головы щучьи с чесноком да свежайшая сёмгушка.');
             setIsActiveProduct(!isActiveProduct)
@@ -16,13 +25,6 @@ export default ({isActive: disabled, category}) => {
             setIsActiveProduct(!isActiveProduct)
         }
     };
-
-    useEffect(() => {
-        if (!disabled) {
-            setProductMessage(`Печалька, с ${category} закончился.`);
-            setProductStatus('product-card_disabled');
-        }
-    }, [])
 
     return [{productStatus, productMessage}, addProduct]
 }
