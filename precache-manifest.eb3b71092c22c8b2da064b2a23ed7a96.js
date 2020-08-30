0bf10f44249193d878bca2ff0d6c113b9c80cf39
DenisRaymer <to.mars.66.miles@gmail.com>
2020-08-30 15:46:55 +0300

    Updates

---
+++ precache-manifest.eb3b71092c22c8b2da064b2a23ed7a96.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d13c16aa8e54fef23438ce8963985ee",
+    "url": "/funbox-kitty/index.html"
+  },
+  {
+    "revision": "39217822fdc985868b8c",
+    "url": "/funbox-kitty/static/css/main.c3837bb3.chunk.css"
+  },
+  {
+    "revision": "6738890239360123b04a",
+    "url": "/funbox-kitty/static/js/2.1a691ca3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/funbox-kitty/static/js/2.1a691ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39217822fdc985868b8c",
+    "url": "/funbox-kitty/static/js/main.43b1e15e.chunk.js"
+  },
+  {
+    "revision": "1655caaa009f20ee33a8",
+    "url": "/funbox-kitty/static/js/runtime-main.3fd63ac9.js"
+  },
+  {
+    "revision": "d486fd221968277f43932575c8cee0c7",
+    "url": "/funbox-kitty/static/media/btn-dashed.d486fd22.svg"
+  },
+  {
+    "revision": "c6174e886a3ea223a4799e8d6dfcf7f5",
+    "url": "/funbox-kitty/static/media/exo-thin.c6174e88.ttf"
+  },
+  {
+    "revision": "a1f226949959b5535636450a0c17fb4c",
+    "url": "/funbox-kitty/static/media/product-kitty.a1f22694.png"
+  }
+]);
